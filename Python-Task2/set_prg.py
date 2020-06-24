4078f39f494cd59d3cd869a2897d705108c7caf3
Nanak Jaswani <jaswaninanak@gmail.com>
2020-06-24 13:57:22 +0530

    Data Structures in Python

--- Python-Task2/set_prg.py
+++ Python-Task2/set_prg.py
@@ -1,8 +1,24 @@
-#sets and its method
+#sets and its method.
 
 #creating a set
-Stationary = {"pen" , "paper" , "pencil" , "eraser"}
-print("set of stationary",Stationary)
+Stationary = {"pen" , "paper" , "pencil" , "eraser", "marker"}
+print("set1\n",Stationary)
+
+#creating a new set
+ink = {"black" , "blue" , "red", "marker"}
+print("set2\n",ink)
+
+#intersection
+Inter=Stationary.intersection(ink)
+print("Intersection of two sets",Inter) #The intersection() method returns a set that contains the similarity between two or more sets.
+
+#difference
+Diff=Stationary.difference(ink)
+print("Difference between two sets",Diff) #The difference() method returns a set that contains the difference between two sets.
+
+#update
+Stationary.update(ink)
+print("updated set",Stationary) #If an item is present in both sets, only one appearance of this item will be present in the updated set.
 
 #add
 Stationary.add("shopner")
@@ -11,21 +27,18 @@ print("shopner added to stationary",Stationary)
 #remove the specific item/element
 #discard/remove both can be used to remove item
 #if the item which is to be remove is not present then remove will raise an error ,discard will not.
-Stationary.discard("pen")  
-print("pen is removed",Stationary)
-
+Stationary.discard("shopner")  
+print("shopner is removed",Stationary)
 
 #check if element is present in set
-print("paper" in Stationary)  #in is used to check the element is present or not
+print("paper" in Stationary)  #in is used to check the element is present or not if present it will print true else false
 
 #len is used to print the length of the set
 print(len(Stationary))
 
-#new set
-ink = {"black" , "blue" , "red"}
 #union returns new set with all item from both the set
 NewSet = Stationary.union(ink)
-print(NewSet)
+print("New Set is=",NewSet)
 
 #clear() is used to empty the set
 Stationary.clear()
@@ -34,3 +47,6 @@ print(Stationary)
 #del is used to delete the set completely
 del Stationary
 print(Stationary)
+
+
+
---
+++ readme.md
@@ -0,0 +1,21 @@
+Codewayy_Python_Series
+Task1
+Here goes the first task of Python_Series!
+
+This Repo is created for begineer's you will find the basic code in which you will get the concept of integers,float,variables. and an string program with concatination.
+
+Task2
+In this task you will learn about the non primitive data structure-
+
+1.List
+
+2.Tuple
+
+3.Set
+
+4.Dictinoaries
+
+In above task you will find the programs on this data structure's and various method of them.
+
+And some Basic Questions with Answer's on them
+
