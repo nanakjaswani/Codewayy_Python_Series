72530b99a784bcb60a5281f157d2d9189703acfd
Nanak Jaswani <jaswaninanak@gmail.com>
2020-06-24 12:26:35 +0530

    Data Structures in Python

--- Python-Task2/list_prg.py
+++ Python-Task2/list_prg.py
@@ -1,48 +1,48 @@
 #Methods used with list
 
 #Creating a list
-colors = [ 'red' , 'blue' , 'green' , 'yellow']
-print("Colors in Rainbow",colors)
+Colors = [ 'red' , 'blue' , 'green' , 'yellow']
+print("Colors in Rainbow",Colors)
 
 #The append() method adds a single item to the existing list.
 #The item will be added to the end of list.
-colors.append('indigo')
-print("color added to rainbow",colors)
+Colors.append('indigo')
+print("Color added to rainbow",Colors)
 
 #insert() is used to insert the element/item at given index
-colors.insert(2,'orange')
-print("List after inserting the color orange",colors)
+Colors.insert(2,'orange')
+print("List after inserting the color orange",Colors)
 
 #remove() removes a given object from the list.
-colors.remove('orange')
-print("List after removing the color orange",colors)
+Colors.remove('orange')
+print("List after removing the color orange",Colors)
 
 #pop() removes and returns the value of given index.
-colors.pop(4)
-print("\n popping color indigo",colors)
+Colors.pop(4)
+print("\n popping color indigo",Colors)
 
 #pop() without index will remove and return last item/element of list
-colors.pop()
-print("popping without index",colors)
+Colors.pop()
+print("popping without index",Colors)
 
 #extend() function adds the specified list element to the end of current list.
 Rainbow = [ 'orange' , 'indigo' , 'violet' , 'yellow']
-colors.extend(Rainbow)
-print("\n After Extending-",colors)
+Colors.extend(Rainbow)
+print("\n After Extending-",Colors)
 
 #reverse() is used to reverse the content of list.
-colors.reverse()
-print("Reversed List -\n",colors)
+Colors.reverse()
+print("Reversed List -\n",Colors)
 
 #count() returns the count of given element in a list.
-count = colors.count('red')
+count = Colors.count('red')
 print("Number of counts",count) #output=1
-count = colors.count('black')
+count = Colors.count('black')
 print("Number of counts",count) #output=0
 
 #copy the content of one list to another
 #Here content of list colors is being copied to new list called Rainbow.
-Rainbow = colors
+Rainbow = Colors
 print("\n New List Rainbow =",Rainbow)
 
 #sort() allows use to sort the list in ascending order.
--- Python-Task2/set_prg.py
+++ Python-Task2/set_prg.py
@@ -1,36 +1,36 @@
 #sets and its method
 
 #creating a set
-stationary = {"pen" , "paper" , "pencil" , "eraser"}
-print("set of stationary",stationary)
+Stationary = {"pen" , "paper" , "pencil" , "eraser"}
+print("set of stationary",Stationary)
 
 #add
-stationary.add("shopner")
-print("shopner added to stationary",stationary)
+Stationary.add("shopner")
+print("shopner added to stationary",Stationary)
 
 #remove the specific item/element
 #discard/remove both can be used to remove item
 #if the item which is to be remove is not present then remove will raise an error ,discard will not.
-stationary.discard("pen")  
-print("pen is removed",stationary)
+Stationary.discard("pen")  
+print("pen is removed",Stationary)
 
 
 #check if element is present in set
-print("paper" in stationary)  #in is used to check the element is present or not
+print("paper" in Stationary)  #in is used to check the element is present or not
 
 #len is used to print the length of the set
-print(len(stationary))
+print(len(Stationary))
 
 #new set
 ink = {"black" , "blue" , "red"}
 #union returns new set with all item from both the set
-NewSet = stationary.union(ink)
+NewSet = Stationary.union(ink)
 print(NewSet)
 
 #clear() is used to empty the set
-stationary.clear()
-print(stationary)
+Stationary.clear()
+print(Stationary)
 
 #del is used to delete the set completely
-del stationary
-print(stationary)
+del Stationary
+print(Stationary)
